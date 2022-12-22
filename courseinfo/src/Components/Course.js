e4e47ab17f13d411ebd89fe0e577ebd5502657fa
aum1618 <muhammadumerabbas2005@gmail.com>
2022-12-22 15:15:09 +0500

    exercise 2.2 done

--- courseinfo/src/Components/Course.js
+++ courseinfo/src/Components/Course.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Content from './Content'
 import Header from './Header'
+import Total from './Total'
 
 export default function Course({course,parts}) {
   return (
     <div>
          <Header course={course} />
       <Content parts={parts} />
+      <Total one={parts[0].exercises} two={parts[1].exercises} three={parts[2].exercises} />
     </div>
   )
 }
---
+++ courseinfo/src/Components/Total.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Total({one,two,three}) {
+  return (
+    <div>Total of {one+two+three} exercises</div>
+  )
+}
