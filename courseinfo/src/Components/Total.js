1ba944a34c5ff9a9dea00035b6f397fea55e155b
aum1618 <muhammadumerabbas2005@gmail.com>
2022-12-22 15:21:49 +0500

    exercise 2.4 done

--- courseinfo/src/Components/Total.js
+++ courseinfo/src/Components/Total.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Total({one,two,three}) {
   return (
-    <div>Total of {one+two+three} exercises</div>
+    <div>Total of {[one, two, three ].reduce((a, b) => a + b, 0)} exercises</div>
   )
 }
