d402be5078f3806fe05f6265fb090d9f1a0d59ae
aum1618 <muhammadumerabbas2005@gmail.com>
2022-12-26 21:37:34 +0500

    exercise 2.9 done

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,46 +1,69 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function NameList() {
   // Declare a state variable called "names" with an initial value of an empty array
   const [names, setNames] = useState([]);
+  // Declare a state variable called "filterList" with an initial value of an empty array
+  const [filterList, setFilterList] = useState([]);
 
   // Declare a state variable called "name" with an initial value of an empty string
-  const [name, setName] = useState('');
-  const [number,setNumber]=useState('')
+  const [name, setName] = useState("");
+  const [number, setNumber] = useState("");
+  // Declare a state variable called "filter" with an initial value of an empty string
+  const [filter, setFilter] = useState("");
 
   // Event handler for the input element's "onChange" event
   function handleChange(event) {
     setName(event.target.value);
   }
+
   function handleNumber(event) {
     setNumber(event.target.value);
   }
 
+  // Event handler for the filter input element's "onChange" event
+  function handleFilter(event) {
+    setFilter(event.target.value);
+    updateFilterList();
+  }
+
+  // Function to update the filterList state variable based on the current filter value
+  function updateFilterList() {
+    const filteredNames = names.filter((name) => name.name.includes(filter));
+    setFilterList(filteredNames);
+  }
 
   // Event handler for the form's "onSubmit" event
   function handleSubmit(event) {
     event.preventDefault();
-    if(names.name===name){
-      alert(`${name} is already added to phonebook`)
-    }else{
+    if (names.some((name) => names.name === name)) {
+      alert(`${name} is already added to phonebook`);
+    } else {
+      // Add the current value of "name" and "number" to the "names" array as an object
+      const data = { name: name, number: number };
+      setNames([...names, data]);
 
-    // Add the current value of "name" to the "names" array
-    let Data={name:name,number:number}
-    setNames([...names,Data]);
-
-    // Reset the value of "name" to an empty string
-    setName('');
-    setNumber('')
+      // Reset the value of "name" and "number" to empty strings
+      setName("");
+      setNumber("");
     }
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <label>
+        Filter shown with:
+        <input type="text" value={filter} onChange={handleFilter} />
+        <br />
+      </label>
       <form onSubmit={handleSubmit}>
+        <h2>Add a New</h2>
         <label>
           Name:
-          <input type="text" value={name} onChange={handleChange} /><br />
+          <input type="text" value={name} onChange={handleChange} />
+          <br />
+          <br />
           Number:
           <input type="text" value={number} onChange={handleNumber} />
         </label>
@@ -49,7 +72,17 @@ function NameList() {
       <h2>Numbers</h2>
       <div>
         {names.map((name, index) => (
-          <p key={index}>{name.name} {name.number}</p>
+          <p key={index}>
+            {name.name} {name.number}
+          </p>
+        ))}
+      </div>
+      <h2>Filtered list</h2>
+      <div>
+        {filterList.map((name, index) => (
+          <p key={index}>
+            {name.name} {name.number}
+          </p>
         ))}
       </div>
     </div>
