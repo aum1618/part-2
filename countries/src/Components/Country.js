3cdce8fc64b8b5cc6a213ad4140358c2c0213c20
aum1618 <muhammadumerabbas2005@gmail.com>
2022-12-27 20:11:17 +0500

    exercise 2.14 done

--- countries/src/Components/Country.js
+++ countries/src/Components/Country.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import { useState } from 'react';
+import { useEffect } from 'react';
+import axios from "axios";
+
 
 export default function ({name,capital,area,languages,flag}) {
-    const value= Object.values(languages)
+  const [weather,setWeather]=useState([])
+  const API_KEY='9357414232db496584d31327221812'
+  useEffect(() => {
+    const promise = axios
+      .get(`https://api.weatherapi.com/v1/current.json?key=9357414232db496584d31327221812&q=London`)
+      .then((response) => {
+        setWeather(response.data);
+        console.log(weather)
+      });
+  }, []);
+  const value= Object.values(languages)
     return (
     <div>
         <h2>{name}</h2>
@@ -14,6 +28,10 @@ export default function ({name,capital,area,languages,flag}) {
         })}
         </ul>
         <img src={flag} />
+        <h2>Weather of {capital}</h2>
+        {weather.length > 0 ? <p>the temperature is {weather.current.temp_c} °C</p> : null}
+        {weather.length > 0 ? <p>Wind speed {weather.current.wind_kph} °C</p> : null}
+
 
         
 
